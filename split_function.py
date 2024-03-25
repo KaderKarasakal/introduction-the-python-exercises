e6c70bb09e08c43cf328a37f16c66e0796ad45e8
kader karasakal <80412588+KaderKarasakal@users.noreply.github.com>
2024-03-25 03:40:45 +0300

    split_function.py
    oveall

---
+++ split_function.py
@@ -0,0 +1,18 @@
+#dosyadan okuma ve işleme
+def process_file(filename):
+
+    words_list = list()
+    with open(filename, 'r') as file:
+        for line in file:
+            words = line.split()
+            for word in words:
+                if word not in words_list:
+                    words_list.append(word)
+
+    words_list.sort()
+    return words_list
+
+filename = 'romeo.txt'
+
+resulting_words = process_file(filename)
+print(resulting_words)
